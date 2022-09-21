d4f24d39a29983d3ed3903d64c0e14439307c43e
mhilmir <rusydiansyahhilmi@gmail.com>
2022-09-21 13:07:09 +0700

    Alhamdulillah sudah selesai tugas 1

--- src/cobaAja.cpp
+++ src/cobaAja.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <dos.h>
 
-int main()
-{
-    std::cout << "helloWorld" << std::endl;
-    
+int main(){
+    float body[5][2] = {{0,0}, {1,1}, {0.5,5}, {-0.5,5}, {-1,1}};
+    std::cout << sizeof(body)/sizeof(float) << std::endl;
     return 0;
 }
--- src/game3_editMoveFlow.cpp
+++ src/game3_editMoveFlow.cpp
@@ -2,6 +2,9 @@
 #include<conio.h>
 #include<dos.h>
 #include<stdio.h>
+#include<cmath>
+
+#define PI 3.14159265359
 
 class Pesawat{
 private :
@@ -12,6 +15,8 @@ private :
     int Ymax;  // screen y max
     int sty;  // determine plane speed y
     int stx;  // determine plane speed x
+    float body[5][2] = {{0,0}, {1,1}, {0.5,5}, {-0.5,5}, {-1,1}};  // for drawing body plane
+    float wing[4][2] = {{3,1}, {3,2}, {-3,2}, {-3,1}};  // for drawing wing plane
 
 public:
     Pesawat(){
@@ -40,29 +45,79 @@ public:
         }
     }
 
-    void MoveRightBeta(){
+    void MoveRight(){
         x = x + stx;
         if (x > Xmax){
             x = x - Xmax;
         }
     }
 
+    void RotatePesawat(float degree){
+        // convert degree to radian
+        float rad = degree * (PI/180);
+
+        // rotating body
+        for(int i=0; i<5; i++){
+            float temp[2];  // for storing one-point coordinate
+            // assign to temp
+            for(int j=0; j<2; j++){
+                temp[j] = body[i][j];
+            }
+            // convert cartesian to polar (r, theta)
+            float r = sqrt( pow(temp[0],2) + pow(temp[1],2) );
+            float theta = atan2(temp[1], temp[0]);
+
+            // adding theta by degree(radian) inputted
+            theta = theta + rad;
+
+            // convert polar to cartesian again and store it to temp
+            temp[0] = r * cos(theta);  // as x
+            temp[1] = r * sin(theta);  // as y
+
+            // return temp to body_array
+            for(int j=0; j<2; j++){
+                body[i][j] = temp[j];
+            }
+        }
+
+        // rotating wing
+        for(int i=0; i<4; i++){
+            float temp[2];  // for storing one-point coordinate
+            // assign to temp
+            for(int j=0; j<2; j++){
+                temp[j] = wing[i][j];
+            }
+            // convert cartesian to polar (r, theta)
+            float r = sqrt( pow(temp[0],2) + pow(temp[1],2) );
+            float theta = atan2(temp[1], temp[0]);
+
+            // adding theta by degree(radian) inputted
+            theta = theta + rad;
+
+            // convert polar to cartesian again and store it to temp
+            temp[0] = r * cos(theta);  // as x
+            temp[1] = r * sin(theta);  // as y
+
+            // return temp to wing_array
+            for(int j=0; j<2; j++){
+                wing[i][j] = temp[j];
+            }
+        }
+    }
+
     void DrawPesawat(){
-        line((0*sc + x), (0*sc + y), (1*sc + x), (1*sc + y));
-        line((1*sc + x), (1*sc + y), (0.5*sc + x), (5*sc + y));
-        line((0.5*sc + x), (5*sc + y), (0*sc + x), (5*sc + y));
+        // drawing body of plane
+        line((body[0][0]*sc + x), (body[0][1]*sc + y), (body[1][0]*sc + x), (body[1][1]*sc + y));
+        line((body[1][0]*sc + x), (body[1][1]*sc + y), (body[2][0]*sc + x), (body[2][1]*sc + y));
+        line((body[2][0]*sc + x), (body[2][1]*sc + y), (body[3][0]*sc + x), (body[3][1]*sc + y));
+        line((body[3][0]*sc + x), (body[3][1]*sc + y), (body[4][0]*sc + x), (body[4][1]*sc + y));
+        line((body[4][0]*sc + x), (body[4][1]*sc + y), (body[0][0]*sc + x), (body[0][1]*sc + y));
 
-        line((0*sc + x), (1*sc + y), (3*sc + x), (1*sc + y));
-        line((3*sc + x), (1*sc + y), (3*sc + x), (2*sc + y));
-        line((3*sc + x), (2*sc + y), (0*sc + x), (2*sc + y));
-
-        line((0*sc + x), (0*sc + y), (-1*sc + x), (1*sc + y));
-        line((-1*sc + x), (1*sc + y), (-0.5*sc + x), (5*sc + y));
-        line((-0.5*sc + x), (5*sc + y), (0*sc + x), (5*sc + y));
-
-        line((0*sc + x), (1*sc + y), (-3*sc + x), (1*sc + y));
-        line((-3*sc + x), (1*sc + y), (-3*sc + x), (2*sc + y));
-        line((-3*sc + x), (2*sc + y), (0*sc + x), (2*sc + y));
+        // drawing wing of plane
+        line((wing[0][0]*sc + x), (wing[0][1]*sc + y), (wing[1][0]*sc + x), (wing[1][1]*sc + y));
+        line((wing[1][0]*sc + x), (wing[1][1]*sc + y), (wing[2][0]*sc + x), (wing[2][1]*sc + y));
+        line((wing[2][0]*sc + x), (wing[2][1]*sc + y), (wing[3][0]*sc + x), (wing[3][1]*sc + y));
+        line((wing[3][0]*sc + x), (wing[3][1]*sc + y), (wing[0][0]*sc + x), (wing[0][1]*sc + y));
     }
 };
 
@@ -74,6 +129,12 @@ int main()
     Pesawat M[20];
     float xh,yh;
     char c;
+
+    // harus dijalankan cuma sekali
+    P.RotatePesawat(90);
+    for(int i=0; i<20; i++){
+        M[i].RotatePesawat(90);
+    }
 
     do{
         cleardevice();
@@ -93,9 +154,9 @@ int main()
         //     M[i].DrawPesawat();
         // }
 
-        // move pesawat ke kanan betaa
+        // move pesawat ke kanan
         for(int i=0; i<20; i++){
-            M[i].MoveRightBeta();
+            M[i].MoveRight();
             M[i].DrawPesawat();
         }
 
