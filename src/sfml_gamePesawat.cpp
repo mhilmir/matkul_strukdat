a0193de507dfd91a0255c13be2466748a6a11881
hilmiubuntu <rusydiansyahhilmi@gmail.com>
2022-10-05 12:59:54 +0700

    pesawat hancur beta

--- src/sfml_gamePesawat.cpp
+++ src/sfml_gamePesawat.cpp
@@ -19,6 +19,8 @@ private :
     float wing_points[6][2] = {{1,-2}, {5,0}, {4,1}, {-4,1}, {-5,0}, {-1,-2}};  // for drawing wing_points plane
     float tole_box;
     float health;
+    float randValue_forBody[13];
+    float randValue_forWing[6];
 
 public:
     PesawatLakon(){
@@ -27,6 +29,13 @@ public:
         pos_y = height_screen * 0.75;
         tole_box = 2.5;
         health = 100;
+
+        for(int i=0; i<13; i++){
+            randValue_forBody[i] = (rand()%3) * scale;  // hmmm kayake randome tetep terus deh
+        }
+        for(int i=0; i<6; i++){
+            randValue_forWing[i] = (rand()%3) * scale;
+        }
     }
 
     void moveHor(int speed){
@@ -69,34 +78,129 @@ public:
     }
 
     void drawPesawatHancur(sf::RenderWindow& window){
-        sf::Vertex draw_body[] = {
-            sf::Vertex(sf::Vector2f((pos_x + body_points[0][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[0][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[1][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[1][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[2][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[2][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[3][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[3][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[4][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[4][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[5][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[5][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[6][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[6][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[7][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[7][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[8][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[8][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[9][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[9][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[10][0]*scale) + (rand()%3)* scale,                     ( pos_y + body_points[10][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[11][0]*scale) + (rand()%3)* scale,                     ( pos_y + body_points[11][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[12][0]*scale) + (rand()%3)* scale,                     ( pos_y + body_points[12][1]*scale) + (rand()%3)* scale)),
-            sf::Vertex(sf::Vector2f((pos_x + body_points[0][0]*scale) + (rand()%3)* scale,                      (pos_y + body_points[0][1]*scale) + (rand()%3)* scale))
+        // ini asalnya body
+        sf::Vertex draw_line1[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[0][0]*scale) + randValue_forBody[0],                      (pos_y + body_points[0][1]*scale) + randValue_forBody[0])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[1][0]*scale) + randValue_forBody[0],                      (pos_y + body_points[1][1]*scale) + randValue_forBody[0]))
         };
-        sf::Vertex draw_wing[] = {
-            sf::Vertex(sf::Vector2f(pos_x + wing_points[0][0]*scale, pos_y + wing_points[0][1]*scale)),
-            sf::Vertex(sf::Vector2f(pos_x + wing_points[1][0]*scale, pos_y + wing_points[1][1]*scale)),
-            sf::Vertex(sf::Vector2f(pos_x + wing_points[2][0]*scale, pos_y + wing_points[2][1]*scale)),
-            sf::Vertex(sf::Vector2f(pos_x + wing_points[3][0]*scale, pos_y + wing_points[3][1]*scale)),
-            sf::Vertex(sf::Vector2f(pos_x + wing_points[4][0]*scale, pos_y + wing_points[4][1]*scale)),
-            sf::Vertex(sf::Vector2f(pos_x + wing_points[5][0]*scale, pos_y + wing_points[5][1]*scale)),
-            sf::Vertex(sf::Vector2f(pos_x + wing_points[0][0]*scale, pos_y + wing_points[0][1]*scale))
+        sf::Vertex draw_line2[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[1][0]*scale) + randValue_forBody[1],                      (pos_y + body_points[1][1]*scale) + randValue_forBody[1])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[2][0]*scale) + randValue_forBody[1],                      (pos_y + body_points[2][1]*scale) + randValue_forBody[1]))
         };
+        sf::Vertex draw_line3[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[2][0]*scale) - randValue_forBody[2],                      (pos_y + body_points[2][1]*scale) + randValue_forBody[2])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[3][0]*scale) - randValue_forBody[2],                      (pos_y + body_points[3][1]*scale) + randValue_forBody[2]))
+        };
+        sf::Vertex draw_line4[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[3][0]*scale) + randValue_forBody[3],                      (pos_y + body_points[3][1]*scale) - randValue_forBody[3])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[4][0]*scale) + randValue_forBody[3],                      (pos_y + body_points[4][1]*scale) - randValue_forBody[3]))
+        };
+        sf::Vertex draw_line5[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[4][0]*scale) - randValue_forBody[4],                      (pos_y + body_points[4][1]*scale) - randValue_forBody[4])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[5][0]*scale) - randValue_forBody[4],                      (pos_y + body_points[5][1]*scale) - randValue_forBody[4]))
+        };
+        sf::Vertex draw_line6[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[5][0]*scale) + randValue_forBody[5],                      (pos_y + body_points[5][1]*scale) - randValue_forBody[5])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[6][0]*scale) + randValue_forBody[5],                      (pos_y + body_points[6][1]*scale) - randValue_forBody[5]))
+        };
+        sf::Vertex draw_line7[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[6][0]*scale) + randValue_forBody[6],                      (pos_y + body_points[6][1]*scale) + randValue_forBody[6])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[7][0]*scale) + randValue_forBody[6],                      (pos_y + body_points[7][1]*scale) + randValue_forBody[6]))
+        };
+        sf::Vertex draw_line8[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[7][0]*scale) - randValue_forBody[7],                      (pos_y + body_points[7][1]*scale) + randValue_forBody[7])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[8][0]*scale) - randValue_forBody[7],                      (pos_y + body_points[8][1]*scale) + randValue_forBody[7]))
+        };
+        sf::Vertex draw_line9[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[8][0]*scale) - randValue_forBody[8],                      (pos_y + body_points[8][1]*scale) - randValue_forBody[8])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[9][0]*scale) - randValue_forBody[8],                      (pos_y + body_points[9][1]*scale) - randValue_forBody[8]))
+        };
+        sf::Vertex draw_line10[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[9][0]*scale) + randValue_forBody[9],                      (pos_y + body_points[9][1]*scale) + randValue_forBody[9])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[10][0]*scale) + randValue_forBody[9],                     (pos_y + body_points[10][1]*scale) + randValue_forBody[9]))
+        };
+        sf::Vertex draw_line11[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[10][0]*scale) + randValue_forBody[10],                     ( pos_y + body_points[10][1]*scale) - randValue_forBody[10])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[11][0]*scale) + randValue_forBody[10],                     ( pos_y + body_points[11][1]*scale) - randValue_forBody[10]))
+        };
+        sf::Vertex draw_line12[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[11][0]*scale) - randValue_forBody[11],                     ( pos_y + body_points[11][1]*scale) - randValue_forBody[11])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[12][0]*scale) - randValue_forBody[11],                     ( pos_y + body_points[12][1]*scale) - randValue_forBody[11]))
+        };
+        sf::Vertex draw_line13[] = {
+            sf::Vertex(sf::Vector2f((pos_x + body_points[12][0]*scale) - randValue_forBody[12],                     ( pos_y + body_points[12][1]*scale) + randValue_forBody[12])),
+            sf::Vertex(sf::Vector2f((pos_x + body_points[0][0]*scale) - randValue_forBody[12],                      (pos_y + body_points[0][1]*scale) + randValue_forBody[12]))
+        };
+        //ini asalnya wing
+        sf::Vertex draw_line14[] = {
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[0][0]*scale) + randValue_forWing[0],                      (pos_y + wing_points[0][1]*scale) + randValue_forWing[0])),
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[1][0]*scale) + randValue_forWing[0],                      (pos_y + wing_points[1][1]*scale) + randValue_forWing[0]))
+        };
+        sf::Vertex draw_line15[] = {
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[1][0]*scale) + randValue_forWing[1],                      (pos_y + wing_points[1][1]*scale) + randValue_forWing[1])),
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[2][0]*scale) + randValue_forWing[1],                      (pos_y + wing_points[2][1]*scale) + randValue_forWing[1]))
+        };
+        sf::Vertex draw_line16[] = {
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[2][0]*scale) - randValue_forWing[2],                      (pos_y + wing_points[2][1]*scale) + randValue_forWing[2])),
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[3][0]*scale) - randValue_forWing[2],                      (pos_y + wing_points[3][1]*scale) + randValue_forWing[2]))
+        };
+        sf::Vertex draw_line17[] = {
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[3][0]*scale) + randValue_forWing[3],                      (pos_y + wing_points[3][1]*scale) - randValue_forWing[3])),
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[4][0]*scale) + randValue_forWing[3],                      (pos_y + wing_points[4][1]*scale) - randValue_forWing[3]))
+        };
+        sf::Vertex draw_line18[] = {
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[4][0]*scale) - randValue_forWing[4],                      (pos_y + wing_points[4][1]*scale) - randValue_forWing[4])),
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[5][0]*scale) - randValue_forWing[4],                      (pos_y + wing_points[5][1]*scale) - randValue_forWing[4]))
+        };
+        sf::Vertex draw_line19[] = {
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[5][0]*scale) + randValue_forWing[5],                      (pos_y + wing_points[5][1]*scale) - randValue_forWing[5])),
+            sf::Vertex(sf::Vector2f((pos_x + wing_points[0][0]*scale) + randValue_forWing[5],                      (pos_y + wing_points[0][1]*scale) - randValue_forWing[5]))
+        };
+        // sf::Vertex draw_wing[] = {
+            // sf::Vertex(sf::Vector2f(pos_x + wing_points[0][0]*scale, pos_y + wing_points[0][1]*scale)),
+            // sf::Vertex(sf::Vector2f(pos_x + wing_points[1][0]*scale, pos_y + wing_points[1][1]*scale)),
+            // sf::Vertex(sf::Vector2f(pos_x + wing_points[2][0]*scale, pos_y + wing_points[2][1]*scale)),
+            // sf::Vertex(sf::Vector2f(pos_x + wing_points[3][0]*scale, pos_y + wing_points[3][1]*scale)),
+            // sf::Vertex(sf::Vector2f(pos_x + wing_points[4][0]*scale, pos_y + wing_points[4][1]*scale)),
+            // sf::Vertex(sf::Vector2f(pos_x + wing_points[5][0]*scale, pos_y + wing_points[5][1]*scale)),
+            // sf::Vertex(sf::Vector2f(pos_x + wing_points[0][0]*scale, pos_y + wing_points[0][1]*scale))
+        // };
 
-        window.draw(draw_body, 13+1, sf::LineStrip);
-        window.draw(draw_wing, 6+1, sf::LineStrip);
+        window.draw(draw_line1, 2, sf::Lines);
+        window.draw(draw_line2, 2, sf::Lines);
+        window.draw(draw_line3, 2, sf::Lines);
+        window.draw(draw_line4, 2, sf::Lines);
+        window.draw(draw_line5, 2, sf::Lines);
+        window.draw(draw_line6, 2, sf::Lines);
+        window.draw(draw_line7, 2, sf::Lines);
+        window.draw(draw_line8, 2, sf::Lines);
+        window.draw(draw_line9, 2, sf::Lines);
+        window.draw(draw_line10, 2, sf::Lines);
+        window.draw(draw_line11, 2, sf::Lines);
+        window.draw(draw_line12, 2, sf::Lines);
+        window.draw(draw_line13, 2, sf::Lines);
+        window.draw(draw_line14, 2, sf::Lines);
+        window.draw(draw_line15, 2, sf::Lines);
+        window.draw(draw_line16, 2, sf::Lines);
+        window.draw(draw_line17, 2, sf::Lines);
+        window.draw(draw_line18, 2, sf::Lines);
+        window.draw(draw_line19, 2, sf::Lines);
+        // window.draw(draw_wing, 6+1, sf::LineStrip);
+
+        for(int i=0; i<13; i++){
+            if(i%2==0){
+                randValue_forBody[i] += 1*0.025*scale;
+            } else{
+                randValue_forBody[i] -= 1*0.025*scale;
+            }
+        }
+        for(int i=0; i<6; i++){
+            if(i%2==0){
+                randValue_forWing[i] += 1*0.025*scale;
+            } else{
+                randValue_forWing[i] -= 1*0.025*scale;
+            }
+        }
     }
 
     float batasBox_kiri(){
@@ -113,7 +217,11 @@ public:
     }
 
     void reduceHealth(float damage){
-        health = health + damage;
+        if(health <= 0){
+            health = 0;
+        } else{
+            health = health + damage;
+        }
     }
 
     void displayHealth(sf::RenderWindow& window){
@@ -339,7 +447,7 @@ int main(){
                 opponents[i].posBulletY() >= jatayu.batasBox_atas() &&
                 opponents[i].posBulletY() <= jatayu.batasBox_bawah()){
                     opponents[i].setBulletExistance(false);
-                    jatayu.reduceHealth(-5);
+                    jatayu.reduceHealth(-100);
 
             }
         }
